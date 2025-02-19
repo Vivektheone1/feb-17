01a3dc65a99310631490c195d03f4c44aa3e96e3
Vivektheone1 <112014847+Vivektheone1@users.noreply.github.com>
2025-02-19 09:37:50 +0530

    Add files via upload

---
+++ Day 2 assignment 1/Main2.java
@@ -0,0 +1,16 @@
+// check even or odd number
+
+
+class Main2 {
+    public static void main(String[] args) {
+        
+  int i = 17;
+if (i%2==0 ){
+System.out.println("the given number is even");
+}
+else{
+System.out.println("the given number is odd");
+}
+       
+    }
+}
---
+++ Day 2 assignment 1/Main3.java
@@ -0,0 +1,11 @@
+// Area of square
+
+
+class Main3 {
+    public static void main(String[] args) {
+        
+int a =12;
+        System.out.println("area of square is " +  a*a);
+
+    }
+}
